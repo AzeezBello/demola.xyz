4996fab016e72de41824d44f07862a0a1caf9b38
AzeezBello <azeez@scholarx.co>
2020-03-02 11:41:24 +0100

    site is live

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,95 +1,19 @@
 import React from "react";
 import './contact.css';
-import {
-  Container, Col, Form,
-  FormGroup, Label, Input,
-  Button,
-} from 'reactstrap';
+import { ReactTypeformEmbed } from 'react-typeform-embed';
 
 import Navbar from './../Navbar/Navbar';
-import LinkedIN from '../../Img/LinkedIN.svg';
-import Github from '../../Img/Github.svg';
-import Behance from '../../Img/Behance.svg';
-
 
 const Contact = props => (
 
   <div id="contact" className="">
     <Navbar />    
-    <div class="pt-5">
-      <div class="p-5 cf ph2-ns">
-           
-        <div class="row">        
-          <div class="col-sm-6">
-              <div class=" pv4">
-                <div className="icons">
-                  <ul>
-                    <li><img src={Github} className="navbarimage" alt="Github"/><a href='https://github.com/AzeezBello'> Github </a></li>
-                    <li><img src={LinkedIN} className="navbarimage" alt="LinkedIn"/><a href='https://www.linkedin.com/in/azeez-bello-48205a11a'> LinkedIn</a></li>
-                    <li><img src={Behance} className="navbarimage" alt="Behance"/><a href='https://www.behance.net/demolablaze'>Behance</a></li>
-                  </ul>
-                </div>
-              </div>
-          </div>
-          
-          <div class="col-sm-6 p-5">
-          <Container className="App">
-            <h2 className="white">Send me a Message</h2>
-            <Form className="form pt-5">
-            <Col>
-                <FormGroup>
-                  <Label for="examplePassword">Full Name</Label>
-                  <Input
-                    type="text"
-                    name="fullName"
-                    id="fullName"
-                    placeholder="John Doe"
-                  />
-                </FormGroup>
-              </Col>
-              <Col>
-                <FormGroup>
-                  <Label>Email</Label>
-                  <Input
-                    type="email"
-                    name="email"
-                    id="exampleEmail"
-                    placeholder="myemail@email.com"
-                  />
-                </FormGroup>
-              </Col>
-              <Col>
-                <FormGroup>
-                  <Label>Phone Number</Label>
-                  <Input
-                    type="int"
-                    name="phoneNumber"
-                    id="phoneNumber"
-                    placeholder="+2347064463920"
-                  />
-                </FormGroup>
-              </Col>
-              <Col>
-                <FormGroup>
-                  <Label>Message</Label>
-                  <Input
-                    type="textarea"
-                    name="message"
-                    id="message"
-                    placeholder="I would like to ......"
-                  />
-                </FormGroup>
-              </Col>
-              <Button >Submit</Button>
-            </Form>
-          </Container>
-          </div>
-        </div>
-     
-
+    
+    <div class="m-5">
+      <div>
+        <ReactTypeformEmbed url="https://demolablaze.typeform.com/to/vvKMQ0" />
       </div>
     </div>
-
 
   </div>
 );
--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -9,10 +9,9 @@ import Behance from '../../Img/Behance.svg';
 
  
 
-
 const Hero = () => {
-    const switchHeader =["Hello...","I'm Demola Bello","A software engineer and designer"]
-    const switchSubHeader =["Welcome to Demola.xyz","An innovate problem solver","With a track record of providing world class solutions"]
+    const switchHeader =["Hello...","I'm Demola Bello"]
+    const switchSubHeader =["Welcome to Demola.xyz"]
 
     return (        
         <div className="hero">
@@ -23,22 +22,21 @@ const Hero = () => {
                     <ReactTypingEffect
                         className="header-text"
                         text={switchHeader}
-                        speed="130"
-                        eraseDelay="5000"
+                        speed="50"
+                        // eraseDelay="2000"
                     />
 
                     <ReactTypingEffect
                         className="subheader"
                         text={switchSubHeader}
-                        speed="100"
-                        eraseDelay="3000"
+                        speed="50"
                     />
                 
                 </div>  
             </div>
 
             <div className="cta">    
-                <a href="/village" >
+                <a href="/Projects" >
                     <button type="button" className="btn btn-lg btn-primary">View Portfolio</button>
                 </a>
                 <a href="https://drive.google.com/open?id=1juDl-BvM4UF_hPA6j8IpuXWYq7hBUh7R" className="ml-4"><b>Download Resume &rarr;</b></a>     
@@ -46,9 +44,9 @@ const Hero = () => {
 
             <div className="icons">
                 <ul>
-                    <li><a href='https://github.com/AzeezBello'><img src={Github} className="navbarimage" alt="Github"/> </a></li>
-                    <li><a href='https://www.linkedin.com/in/azeez-bello-48205a11a'><img src={LinkedIN} className="navbarimage" alt="LinkedIn"/></a></li>
-                    <li><a href='https://www.behance.net/demolablaze'><img src={Behance} className="navbarimage" alt="Behance"/></a></li>
+                    <li><a href='https://github.com/AzeezBello'><img src={Github}  alt="Github"/> </a></li>
+                    <li><a href='https://www.linkedin.com/in/azeez-bello-48205a11a'><img src={LinkedIN}  alt="LinkedIn"/></a></li>
+                    <li><a href='https://www.behance.net/demolablaze'><img src={Behance} alt="Behance"/></a></li>
                 </ul>
             </div>
             
